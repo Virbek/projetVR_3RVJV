208de68f37bfbcd1ba972f173e31ec685b5d3f5d
Virbek <61600649+Virbek@users.noreply.github.com>
2024-04-14 15:52:47 +0200

    pull

--- Assets/Script/killLight.cs
+++ Assets/Script/killLight.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
 public class killLight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private LayerMask collisionLayer;
 
+    private float timeToKill = 0f;
+    private bool start = false;
+    private float startTime = 0f;
+
+    private GameObject enemy;
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity, collisionLayer))
+        {
+            if (hit.collider.gameObject.CompareTag("enemy"))
+            {
+                start = true;
+                enemy = hit.collider.gameObject;
+            }
+        }else
+        {
+            start = false;
+            timeToKill = 0f;
+            enemy = null;
+        }
+
+        if (start)
+        {
+            timeToKill += Time.deltaTime;
+
+            if (timeToKill >= 2f)
+            {
+                enemy.SetActive(false);
+                timeToKill = 0f;
+                start = false;
+            }
+        }
         
     }
 }
