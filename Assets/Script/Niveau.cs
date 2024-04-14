c1b41eaefe9cf36b1d5ffcfdd50b3ebf982f04f0
Gregory Brusson <brusgregory@gmail.com>
2024-04-14 16:32:11 +0200

    Changer de niveau

---
+++ Assets/Script/Niveau.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Niveau : MonoBehaviour
+{
+    public string NomDeScene;
+
+
+    public void NiveauSuivant()
+    {
+        SceneManager.LoadScene(NomDeScene);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        NiveauSuivant();
+    }
+}
